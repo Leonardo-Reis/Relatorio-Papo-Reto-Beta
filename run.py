fdff682da2e29602d1bec9e44909c767002b57ed
Leonardo Reis <leonardocarlosreis@gmail.com>
2021-05-29 20:44:26 -0300

    Definição da porta e do host

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,5 +8,10 @@ def index():
     return 'Hello, World!'
 
 
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
 if __name__ == '__main__':
-    app.run()
+    main()
