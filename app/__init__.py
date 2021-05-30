0670afdef30400472f3cddd4bcdc00de7acff15d
Leonardo Reis <leonardocarlosreis@gmail.com>
2021-05-29 21:26:20 -0300

    novo init

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'secret-jqv'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 db = SQLAlchemy(app)
